6b1209342e244c29a41fd85adc9752d88a5c018b
Vishwa124-cell <127947206+Vishwa124-cell@users.noreply.github.com>
2024-12-22 12:55:52 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,21 @@
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open('app-shell').then((cache) => {
-      return cache.addAll([
-        '/',
-        'https://thunkable.site/w/Ka6rgFo2vNc_hI1T9NsBs',
-        'icon-192x192.png',
-        'icon-512x512.png'
-      ]);
-    })
-  );
+  console.log('Service Worker installed.');
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker activated.');
 });
 
 self.addEventListener('fetch', (event) => {
+  // Add this to make your site work offline
   event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      return cachedResponse || fetch(event.request);
-    })
+      caches.match(event.request).then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
   );
 });
